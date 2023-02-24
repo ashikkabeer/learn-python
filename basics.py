4710aba57c2b7039415f8361b2ee8cb92f77397c
Ashik Kabeer <ashikkabeer17@gmail.com>
2023-02-24 21:14:04 +0530

    initial commit

---
+++ basics.py
@@ -0,0 +1,15 @@
+#started learning python
+print('hello world')
+
+#primitives
+print(1+1) #simple addition
+print(5//3)    #division result rounded down
+print(2**3) #exponent
+
+print(not True) #negate with `not`
+print(True and False) 
+print(True or False)
+
+#value of true is 1 and false is 0. So True + True gives 2;
+
+print(0==False) #returns true becaus 0==0;
